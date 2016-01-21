4d42c45f31f38c1f0c269aad09b3ec5a903f84ee
Vic Amuso <vic@parablesoft.com>
2016-01-20 19:24:49 -0500

    Removed notable notification

--- addon/components/notable-notification.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/notable-notification';
-
-const {oneWay} = Ember.computed;
-
-export default Ember.Component.extend({
-  layout: layout,
-	isNotifying: oneWay("user.isNotifying"),
-});
--- app/components/notable-notification.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-notable/components/notable-notification';
